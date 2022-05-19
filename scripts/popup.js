b4a76be00820cbf53a51368b212daf1de537a5a6
ChenXiao Liu <810196813@qq.com>
2022-05-18 20:12:49 -0500

    optimize code

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,8 @@
-import colorChanger from "./injections/colorChanger.js";
+import elementSelector from "./injections/elementSelector.js";
 
 const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 const url = new URL(tab.url);
+document.getElementById('website').textContent = url.host;
 const chooseElement = document.getElementById('choose-element');
 const cancel = document.getElementById('cancel');
 
@@ -12,24 +13,23 @@ function toggleHiddenVisible(...elements) {
   })
 }
 
-const { result } = (await chrome.scripting.executeScript({
-  target: { tabId: tab.id },
-  func: () => document.getElementById('color-changer-highlight-div') ? true : false,
-}))[0];
-toggleHiddenVisible(result ? cancel : chooseElement);
-
-document.getElementById('website').textContent = url.host;
-// document.getElementById('page').textContent = url.pathname;
-
 chooseElement.addEventListener('click', () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: colorChanger,
+    func: elementSelector,
   });
   window.close();
-})
+});
 
 cancel.addEventListener('click', async () => {
   await chrome.tabs.sendMessage(tab.id, 'cleanup')
   toggleHiddenVisible(cancel, chooseElement);
-})
+});
+
+(async function () {
+  const [results] = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => document.getElementById('color-changer-highlight-div') ? true : false,
+  })
+  toggleHiddenVisible(results && results.result ? cancel : chooseElement);
+})();
