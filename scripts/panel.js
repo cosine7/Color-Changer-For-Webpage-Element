13a2a756b6e653391feb2ceb42a4769ed6a19901
ChenXiao Liu <810196813@qq.com>
2022-07-29 17:45:58 -0500

    complete color changing function

--- scripts/panel.js
+++ scripts/panel.js
@@ -1,19 +1,34 @@
-chrome.devtools.panels.elements.createSidebarPane('Color Changer', panel => {
-  panel.setPage('public/panel.html');
-});
-
-chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
+function onSelectionChanged() {
   chrome.devtools.inspectedWindow.eval(
     'elementChanged($0)',
     { useContentScriptContext: true },
   );
+}
+
+function colorChanged(property, value) {
+  chrome.devtools.inspectedWindow.eval(
+    `changeStyle('${property}','${value}')`,
+    { useContentScriptContext: true },
+  );
+}
+
+chrome.devtools.panels.elements.createSidebarPane('Color Changer', panel => {
+  panel.setPage('public/panel.html');
 });
 
+chrome.devtools.panels.elements.onSelectionChanged.addListener(onSelectionChanged);
+
 const [header] = document.getElementsByTagName('header');
+const [background, foreground] = document.getElementsByTagName('input');
+
+background.addEventListener('change', () => { colorChanged('backgroundColor', background.value); });
+foreground.addEventListener('change', () => { colorChanged('color', foreground.value); });
 
 const events = {
-  elementChanged({ identifier }) {
+  elementChanged({ identifier, color }) {
     header.textContent = `${identifier.key}: ${identifier.value}`;
+    background.value = color.background;
+    foreground.value = color.foreground;
   },
 };
 
@@ -25,9 +40,4 @@ chrome.runtime.onMessage.addListener(({ event, data }, sender, sendResponse) => 
 chrome.scripting.executeScript({
   target: { tabId: chrome.devtools.inspectedWindow.tabId },
   files: ['scripts/injections/test.js'],
-}, () => {
-  chrome.devtools.inspectedWindow.eval(
-    'elementChanged($0)',
-    { useContentScriptContext: true },
-  );
-});
+}, onSelectionChanged);
